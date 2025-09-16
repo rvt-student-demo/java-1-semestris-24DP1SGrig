8434853d09261946802fc63f1ddeaf14f0afd5f2
Sergejs Grigorjevs <A240026SG@rvt.lv>
2025-09-16 14:11:14 +0300

    Addition formula

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,18 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+        
+        int sum = first + second;
+        System.out.println(first + " + " + second + " = " + sum);
     }
 }
