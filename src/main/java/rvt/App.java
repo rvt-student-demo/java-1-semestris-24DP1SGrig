34f1c4ae8940f139630e866ee450b0ab7b4d6a4a
Sergejs Grigorjevs <A240026SG@rvt.lv>
2025-09-18 12:25:53 +0300

    Odd or even

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -45,4 +45,14 @@ public class App {
         }
         
     }
+    public static void oddOrEven(String[] args) {
+        System.out.println("Give a number:");
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number % 2 == 0) {
+            System.out.println("Number " + number + " is even.");
+        } else {
+            System.out.println("Number " + number + " is odd.");
+        }
+    }
 }
