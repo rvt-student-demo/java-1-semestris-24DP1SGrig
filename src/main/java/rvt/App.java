f19f55a11fa4cf165a7bb01dde606b4386b3a120
Sergejs Grigorjevs <A240026SG@rvt.lv>
2025-09-18 12:25:15 +0300

    Grades and Points

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -4,18 +4,45 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        gradesAndPoints(args);
+        oddOrEven(args);
+        // Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Give the first number:");
-        int first = Integer.valueOf(scanner.nextLine());
+        // System.out.println("Give the first number:");
+        // int first = Integer.valueOf(scanner.nextLine());
         
-        System.out.println("Give the second number:");
-        int second = Integer.valueOf(scanner.nextLine());
+        // System.out.println("Give the second number:");
+        // int second = Integer.valueOf(scanner.nextLine());
         
-        System.out.println("Give the third number:");
-        int third = Integer.valueOf(scanner.nextLine());
+        // System.out.println("Give the third number:");
+        // int third = Integer.valueOf(scanner.nextLine());
 
-        double sum = (first + second + third) / 3.0;
-        System.out.println("The average is " + sum);
+        // double sum = (first + second + third) / 3.0;
+        // System.out.println("The average is " + sum);
+    }
+    public static void gradesAndPoints(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give points [0-100]:");
+        int points = Integer.valueOf(scanner.nextLine());
+
+        if (points < 0) {
+            System.out.println("Grade: impassable!");
+        } else if (points < 50) {
+            System.out.println("Grade: failed");
+        } else if (points < 60) {
+            System.out.println("Grade: 1");
+        } else if (points < 70) {
+            System.out.println("Grade: 2");
+        } else if (points < 80) {
+            System.out.println("Grade: 3");
+        } else if (points < 90) {
+            System.out.println("Grade: 4");
+        } else if (points <= 100) {
+            System.out.println("Grade: 5");
+        } else {
+            System.out.println("Grade: incredible!");
+        }
+        
     }
 }
