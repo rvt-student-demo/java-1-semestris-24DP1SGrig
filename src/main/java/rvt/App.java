d2be08e59d04154a2003704f574e087a5b1f2730
Sergejs Grigorjevs <A240026SG@rvt.lv>
2025-09-23 14:11:49 +0300

    Detaļas

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        giftTax(args);
+        detalas(args);
+        // giftTax(args);
         // gradesAndPoints(args);
         // oddOrEven(args);
         // Scanner scanner = new Scanner(System.in);
@@ -62,6 +63,7 @@ public class App {
         System.out.println("Value of the gift?");
         double value = Double.valueOf(scanner.nextLine());
         double tax;
+
         if (value < 5000) {
             tax = 0;
         } else if (value < 25000) {
@@ -75,6 +77,35 @@ public class App {
         } else {
             tax = 142100 + (value - 1000000) * 0.17;
         }
+
         System.out.println("Tax: " + tax);
+    }
+    public static void detalas(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int SKRUVES_CENA = 5;
+        final int UZGRIEZNIS_CENA = 3;
+        final int PAPLASNE_CENA = 1;
+
+        int skruves = 0;
+        int uzgrieznis = 0;
+        int paplasnes = 0;
+
+        System.out.println("Skrvju skaits:");
+        skruves = Integer.valueOf(scanner.nextLine());
+        System.out.println("Uzgrieznu skaits:");
+        uzgrieznis = Integer.valueOf(scanner.nextLine());
+        System.out.println("Paplasnes skaits:");
+        paplasnes = Integer.valueOf(scanner.nextLine());
+
+        if (skruves <= uzgrieznis && paplasnes / 2 == skruves) {
+            System.out.println("Pasutijums ir kartiba");
+        }
+        if (!(skruves <= uzgrieznis)) {
+            System.out.println("Parbaudi pasutijumu: par maz uzgrieznu");
+        }
+        if (!(paplasnes / 2 == skruves)) {
+            System.out.println("Parbaudi pasutijumu: par maz paplaksne");
+        }
+        System.out.println("Kopeja cena: " + (skruves * SKRUVES_CENA + uzgrieznis * UZGRIEZNIS_CENA + paplasnes * PAPLASNE_CENA));
     }
 }
