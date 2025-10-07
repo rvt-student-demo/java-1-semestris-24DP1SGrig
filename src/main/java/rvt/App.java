c833fef66281116b50f9d34ce0ecfbb82cbb905f
Sergejs Grigorjevs <A240026SG@rvt.lv>
2025-10-07 13:10:05 +0300

    Swap

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,18 +3,6 @@ package rvt;
 public class App {
 
     public static void main(String[] args) {
-        divisibleByThreeInRange(2, 10);
-        // Scanner scanner = new Scanner(System.in);
-        // int end = Integer.valueOf(scanner.nextLine());
-        // for (int start = 0; start < end + 1; start++) {
-        //     System.out.println(start);
-        // }
-    }
-    public static void divisibleByThreeInRange(int beginning, int end) {
-        for (int i = beginning; i < end; i++) {
-            if (i % 3 == 0) {
-                System.out.println(i);
-            }
-        }
+        Double[] dvals = {0.0, 0.5, 1.5, 2.0, 2.5};
     }
 }
